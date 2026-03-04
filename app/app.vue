e456227064cc4955e37b65709c1723922eb1b785
almirLimas <104025405+almirLimas@users.noreply.github.com>
2026-03-04 12:02:06 -0300

    instalação nuxt ui

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <span>oi</span>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UApp>
+      <UButton icon="i-lucide-rocket" size="md" color="primary" variant="solid"
+        >Button</UButton
+      >
+
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+    </UApp>
   </div>
 </template>
