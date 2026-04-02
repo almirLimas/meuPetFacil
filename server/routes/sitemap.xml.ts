48127fec627af16e9f413b7e42246b4ec6c8cb5c
almirLimas <104025405+almirLimas@users.noreply.github.com>
2026-04-02 17:46:41 -0300

    add sitemap

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,25 @@
+export default defineEventHandler((event) => {
+  setHeader(event, "Content-Type", "application/xml");
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.aninpet.com.br/</loc>
+    <lastmod>2026-04-02</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.aninpet.com.br/criar-conta</loc>
+    <lastmod>2026-04-02</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.aninpet.com.br/login</loc>
+    <lastmod>2026-04-02</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+</urlset>`;
+});
